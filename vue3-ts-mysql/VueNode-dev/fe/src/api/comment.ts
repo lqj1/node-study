54b0a9d7afbdafa5dff2b50fc2b8486ca608be68
lqj1 <1104942848@qq.com>
2021-12-17 19:08:32 +0800

    新增一个blog开发的全栈项目

---
+++ vue3-ts-mysql/VueNode-dev/fe/src/api/comment.ts
@@ -0,0 +1,23 @@
+import httpRequest, { ResponseData } from './index';
+import { AxiosPromise } from 'axios';
+
+export const getCouponComment = (couponId: Number): AxiosPromise<ResponseData> => {
+  return httpRequest.request({
+    method: 'get',
+    url: '/tour/comment/get',
+    params: { couponId }
+  });
+}
+
+export const publishComment = (
+  userId: number, 
+  starGrade: number, 
+  commentContent: string, 
+  couponId: number
+): AxiosPromise<ResponseData> => {
+  return httpRequest.request({
+    method: 'post',
+    url: '/tour/comment/publish',
+    data: { userId, starGrade, commentContent, couponId }
+  });
+}
---
+++ vue3-ts-mysql/VueNode-dev/fe/src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from './routes'
+
+const router = createRouter({
+  history: createWebHistory('/tour/'),
+  routes
+})
+
+router.afterEach((to, from) => {
+  document.title = '锦囊团'
+});
+
+export default router
