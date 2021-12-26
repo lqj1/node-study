c410e10090d1a515a0f5bc07f2dcecfecaa796c2
lqj1 <1104942848@qq.com>
2021-12-27 01:14:50 +0800

    add

---
+++ Bzhan/demos/02-spider/server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const https = require('https');
+const cheerio = require('cheerio'); // 后端转成虚拟dom
+// 筛选数据函数
+function filterData(data) {
+  // console.log('data: ', data);
+  const $ = cheerio.load(data);
+  // 再解析相应的dom元素并进行分析
+}
+
+const server = http.createServer((req, res) => {
+  let data = '';
+  https.get('https://www.mi.com/', result => {
+    result.on('data', chunk => {
+      data += chunk;
+      console.log('result-on');
+    });
+    result.on('end', () => {
+      console.log('result-end');
+      filterData(data);
+    });
+  });
+});
+server.listen(8080, () => {
+  console.log('listen on 8080');
+});
--- demos2/test1.js
+++
@@ -1,8 +0,0 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-readline.question(`你叫什么名字？`, name => {
-  console.log(`你好，${name}`);
-  readline.close();
-});
