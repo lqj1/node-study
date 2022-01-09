2cf45dacbaba806bef901e2e2cccd1b4c09f17e9
lqj1 <1104942848@qq.com>
2022-01-10 01:42:56 +0800

    增加node基础知识

---
+++ Bzhan/basic/README.md
@@ -0,0 +1,5 @@
+## node 基础入门笔记
+
+### 视频链接
+
+https://www.bilibili.com/video/BV1hf4y1X7xM?p=1
--- projects/04-在线笔记/myNote/models/models.js
+++ projects/04-在线笔记/myNote/models/models.js
@@ -29,3 +29,33 @@ let noteSchema = new Schema({
 });
 // 导出笔记数据模型
 exports.Article = mongoose.model('Note', noteSchema);
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const mime = require('mime');
+http
+  .createServer(function (req, res) {
+    // 1. 获取用户请求的路径
+    // req.url
+    // /css/index.css(css样式)
+    // /image/index.png(图片)
+    // 2. 获取 public 目录的完整路径
+    let publicDir = path.join(__dirname, 'public');
+    // 3. 根据public的路径和用户请求的路径，最终计算出用户请求的静态资源的完整路径
+    let filename = path.join(publicDir, req.url);
+    // 4. 根据文件完整路径去读取文件
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.end('文件不存在！');
+      }
+      // 通过第三方模块 mime, 来判断不同的请求资源对应的 content-type 的类型
+      // mime.getType('txt') => 'text/plain'
+      // mime.getExtension('text/plain') => 'txt'
+      res.setHeader('Content-Type', mime.getType(filename));
+      res.end(data);
+    });
+  })
+  .listen(8080, function () {
+    console.log('listening on http://localhost:8080');
+  });
